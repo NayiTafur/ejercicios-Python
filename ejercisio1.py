d94837ce28b1a17aa6389b2d810a90c172904769
unknown <SENA@IBADFPCIIFSP162.FormacionSena.RED>
2024-04-18 09:54:58 -0500

    mi primer commit

---
+++ ejercisio1.py
@@ -0,0 +1,24 @@
+a=2
+b=5
+suma=a+b
+print("la suma es: ",suma)
+
+a=7
+b=3
+resta=a-b
+print("la resta es: ",resta)
+
+a=9
+b=6
+producto=a*b
+print("la multiplicacion es: ",producto)
+
+a=8
+b=3
+cociente=a/b
+print("la division es: ",cociente)
+
+a=4
+b=8
+residuo=a%b
+print("la division es: ",residuo)
